8dbb1121825a12711a152fbf7d8241f324337edb
David Vieira <david.vieira@trustly.com>
2022-06-13 22:02:54 +0100

    First commit.

--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -4,6 +4,8 @@ import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
 import java.net.URI;
+import okhttp3.OkHttpClient;
+import org.eclipse.jetty.client.HttpClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockserver.integration.ClientAndServer;
@@ -12,8 +14,16 @@ import org.mockserver.model.ConnectionOptions;
 import org.mockserver.model.HttpResponse;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.HttpComponentsClientHttpConnector;
+import org.springframework.http.client.reactive.JettyClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootTest
 class Demo1ApplicationTests {
@@ -21,20 +31,14 @@ class Demo1ApplicationTests {
   private static final ClientAndServer nettyMockServer = ClientAndServer.startClientAndServer(8080);
 
   @Test
-  void errorWhenEmptyGZIPResponseScenario1() {
+  void emptyGZIPResponse_Scenario_RestTemplate_ApacheHttpClient_Chunked() {
     // GIVEN
-    HttpResponse httpResponse = response()
-        .withStatusCode(HttpStatus.FOUND.value())
-        .withConnectionOptions(ConnectionOptions.connectionOptions()
-            .withSuppressContentLengthHeader(true)
-            .withChunkSize(100))
-        .withHeader("Content-Encoding", "gzip")
-        .withBody(new byte[0]);
-
-    nettyMockServer
-        .when(request().withPath("/"), Times.once())
-        .respond(httpResponse);
-    TestRestTemplate testRestTemplate = new TestRestTemplate();
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withChunkSize(100));
+    TestRestTemplate testRestTemplate = new TestRestTemplate(
+        new RestTemplateBuilder()
+            .requestFactory(HttpComponentsClientHttpRequestFactory.class));
 
     // WHEN
     ResponseEntity<String> response = testRestTemplate.postForEntity(URI.create("http://localhost:8080/"), null, String.class);
@@ -44,26 +48,182 @@ class Demo1ApplicationTests {
   }
 
   @Test
-  void errorWhenEmptyGZIPResponseScenario2() {
+  void emptyGZIPResponse_Scenario_RestTemplate_ApacheHttpClient_CloseSocket() {
     // GIVEN
-    HttpResponse httpResponse = response()
-        .withStatusCode(HttpStatus.FOUND.value())
-        .withConnectionOptions(ConnectionOptions.connectionOptions()
-            .withSuppressContentLengthHeader(true)
-            .withCloseSocket(true))
-        .withHeader("Content-Encoding", "gzip")
-        .withBody(new byte[0]);
-
-    nettyMockServer
-        .when(request().withPath("/"), Times.once())
-        .respond(httpResponse);
-    TestRestTemplate testRestTemplate = new TestRestTemplate();
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withCloseSocket(true));
+    TestRestTemplate testRestTemplate = new TestRestTemplate(
+        new RestTemplateBuilder()
+            .requestFactory(HttpComponentsClientHttpRequestFactory.class));
 
     // WHEN
     ResponseEntity<String> response = testRestTemplate.postForEntity(URI.create("http://localhost:8080/"), null, String.class);
 
     // THEN
     Assertions.assertEquals(HttpStatus.FOUND, response.getStatusCode());
+  }
+
+  @Test
+  void emptyGZIPResponse_Scenario_RestTemplate_OkHttp_Chunked() {
+    // GIVEN
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withChunkSize(100));
+    TestRestTemplate testRestTemplate = new TestRestTemplate(
+        new RestTemplateBuilder()
+            .requestFactory(OkHttp3ClientHttpRequestFactory.class));
+
+    // WHEN
+    ResponseEntity<String> response = testRestTemplate.postForEntity(URI.create("http://localhost:8080/"), null, String.class);
+
+    // THEN
+    Assertions.assertEquals(HttpStatus.FOUND, response.getStatusCode());
+  }
+
+  @Test
+  void emptyGZIPResponse_Scenario_RestTemplate_ApacheOkHttp_CloseSocket() {
+    // GIVEN
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withCloseSocket(true));
+    TestRestTemplate testRestTemplate = new TestRestTemplate(
+        new RestTemplateBuilder()
+            .requestFactory(OkHttp3ClientHttpRequestFactory.class));
+
+    // WHEN
+    ResponseEntity<String> response = testRestTemplate.postForEntity(URI.create("http://localhost:8080/"), null, String.class);
+
+    // THEN
+    Assertions.assertEquals(HttpStatus.FOUND, response.getStatusCode());
+  }
+
+  @Test
+  void emptyGZIPResponse_Scenario_RestTemplate_SimpleJDKRequest_Chunked() {
+    // GIVEN
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withChunkSize(100));
+    TestRestTemplate testRestTemplate = new TestRestTemplate(
+        new RestTemplateBuilder()
+            .requestFactory(SimpleClientHttpRequestFactory.class));
+
+    // WHEN
+    ResponseEntity<String> response = testRestTemplate.postForEntity(URI.create("http://localhost:8080/"), null, String.class);
+
+    // THEN
+    Assertions.assertEquals(HttpStatus.FOUND, response.getStatusCode());
+  }
+
+  @Test
+  void emptyGZIPResponse_Scenario_RestTemplate_SimpleJDKRequest_CloseSocket() {
+    // GIVEN
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withCloseSocket(true));
+    TestRestTemplate testRestTemplate = new TestRestTemplate(
+        new RestTemplateBuilder()
+            .requestFactory(SimpleClientHttpRequestFactory.class));
+
+    // WHEN
+    ResponseEntity<String> response = testRestTemplate.postForEntity(URI.create("http://localhost:8080/"), null, String.class);
+
+    // THEN
+    Assertions.assertEquals(HttpStatus.FOUND, response.getStatusCode());
+  }
+
+  @Test
+  void emptyGZIPResponse_Scenario_WebClient_JettyClient_CloseSocket() {
+    // GIVEN
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withCloseSocket(true));
+
+    // WHEN
+    HttpClient httpClient = new HttpClient();
+    ClientHttpConnector connector = new JettyClientHttpConnector(httpClient);
+
+    String response = WebClient.builder().clientConnector(connector)
+        .baseUrl("http://localhost:8080/").build()
+        .post()
+        .retrieve()
+        .bodyToFlux(String.class)
+        .blockFirst();
+
+    // THEN
+    Assertions.assertNull(response);
+  }
+
+  @Test
+  void emptyGZIPResponse_Scenario_WebClient_JettyClient_Chunked() {
+    // GIVEN
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withChunkSize(100));
+
+    // WHEN
+    HttpClient httpClient = new HttpClient();
+    ClientHttpConnector connector = new JettyClientHttpConnector(httpClient);
+
+    String response = WebClient.builder().clientConnector(connector)
+        .baseUrl("http://localhost:8080/").build()
+        .post()
+        .retrieve()
+        .bodyToFlux(String.class)
+        .blockFirst();
+
+    // THEN
+    Assertions.assertNull(response);
+  }
+
+  @Test
+  void emptyGZIPResponse_Scenario_WebClient_HttpComponents_CloseSocket() {
+    // GIVEN
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withCloseSocket(true));
+
+    // WHEN
+    String response = WebClient.builder().clientConnector(new HttpComponentsClientHttpConnector())
+        .baseUrl("http://localhost:8080/").build()
+        .post()
+        .retrieve()
+        .bodyToFlux(String.class)
+        .blockFirst();
+
+    // THEN
+    Assertions.assertNull(response);
+  }
+
+  @Test
+  void emptyGZIPResponse_Scenario_WebClient_HttpComponents_Chunked() {
+    // GIVEN
+    mockServer(ConnectionOptions.connectionOptions()
+        .withSuppressContentLengthHeader(true)
+        .withChunkSize(100));
+
+    // WHEN
+    String response = WebClient.builder().clientConnector(new HttpComponentsClientHttpConnector())
+        .baseUrl("http://localhost:8080/").build()
+        .post()
+        .retrieve()
+        .bodyToFlux(String.class)
+        .blockFirst();
+
+    // THEN
+    Assertions.assertNull(response);
+  }
+
+  private void mockServer(ConnectionOptions connectionOptions) {
+    HttpResponse httpResponse = response()
+        .withStatusCode(HttpStatus.FOUND.value())
+        .withConnectionOptions(connectionOptions)
+        .withHeader("Content-Encoding", "gzip")
+        .withBody(new byte[0]);
+
+    nettyMockServer
+        .when(request().withPath("/"), Times.once())
+        .respond(httpResponse);
   }
 
 }
